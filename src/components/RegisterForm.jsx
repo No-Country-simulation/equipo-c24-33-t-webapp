82508215284e991e76db0c11bfe780df3f914a20
JocelynAraya <arayameza.jocelyn@gmail.com>
2025-03-06 13:36:29 -0300

    formulario-register2

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,36 +1,51 @@
-import React, { useState } from 'react';
-import { TextField, Button, Box, Typography, Container, FormControlLabel, Checkbox } from '@mui/material';
-import { Google as GoogleIcon } from '@mui/icons-material';
-import axios from 'axios'
+import React, { useState } from "react";
+import { TextField, Button, Box, Typography, Container } from "@mui/material";
+import axios from "axios";
+import "../assets/styles/registerForm.css";
+
 export default function RegisterForm({ onRegister }) {
-  
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
   const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      e.preventDefault()
-      const register= await axios.post("https://stockerback.onrender.com/api/auth/register",
-        {email: email,
-        password: password,
-        name: name,
-      })
-      alert("Usuario registrado correctamente" )
-      if (onRegister) onRegister(register.data); // Llama a la función `onRegister` si
+      const register = await axios.post(
+        "https://stockerback.onrender.com/api/auth/register",
+        {
+          name,
+          email,
+          password,
+        }
+      );
+      alert("Usuario registrado correctamente");
+      if (onRegister) onRegister(register.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-   
   };
 
   return (
-    <Container maxWidth="xs">
-      <Box sx={{ mt: 8, p: 3, boxShadow: 3, borderRadius: 2 }}>
-        <Typography variant="h5" component="h1" gutterBottom>
-          Registrarse
-        </Typography>
-        <form onSubmit={handleSubmit}>
+    <section className="register-container">
+      <div className="register-left">
+        <h1>Stocker</h1>
+        <h2>Comprueba los beneficios de Stocker con una prueba gratuita</h2>
+        <p>No necesitas agregar datos de tarjeta de crédito</p>
+        <div className="register-steps">
+          <div className="register-step-box">1️⃣ Ingresa tus datos</div>
+          <div className="register-step-box">
+            2️⃣ Verifica tu correo electrónico
+          </div>
+          <div className="register-step-box">
+            3️⃣ ¡Listo! Ya puedes usar Stocker
+          </div>
+        </div>
+      </div>
+
+      <div className="register-right">
+        <form className="register-form" onSubmit={handleSubmit}>
+          <h3>¡Empieza a utilizar Stocker!</h3>
           <TextField
             fullWidth
             label="Nombre"
@@ -56,20 +71,30 @@ export default function RegisterForm({ onRegister }) {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          
-          <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
-            Registrarse
+          <Button
+  type="submit"
+  variant="contained"
+  fullWidth
+  sx={{
+    backgroundColor: "#97D9E4",
+    width: "50%",
+    marginTop: "10px",
+    color: "#2a2a2a",
+    padding: "10px 30px",
+    borderRadius: "20px",
+    fontSize: "1rem",
+    fontWeight: "bold",
+    textTransform: "none",
+    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+    "&:hover": {
+      backgroundColor: "#8c95f3",
+    },
+  }}
+>
+            ¡Activar!
           </Button>
         </form>
-        <Button
-          variant="outlined"
-          fullWidth
-          startIcon={<GoogleIcon />}
-          sx={{ mt: 2 }}
-        >
-          Registrarse con Google
-        </Button>
-      </Box>
-    </Container>
+      </div>
+    </section>
   );
 }
