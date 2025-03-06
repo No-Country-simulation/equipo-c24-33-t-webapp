910ed2d1d3696cd0302b3bc1b6b929da1674c488
JocelynAraya <arayameza.jocelyn@gmail.com>
2025-03-06 14:29:06 -0300

    product

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import './App.css';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ProductPage from './pages/ProductPage';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/products" element={<ProductPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductContainer from '../components/Logged/ProductContainer.jsx'
+
+const ProductPage = () => {
+  return (
+<>
+<ProductContainer/>
+</>
+)
+}
+
+export default ProductPage
